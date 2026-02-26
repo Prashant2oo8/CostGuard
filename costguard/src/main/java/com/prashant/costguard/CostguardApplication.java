8e54a7161449e0c4621e405fe8ac74091f03e7d0
Prashant Bhardwaj <prashantbhardwaj0107@gmail.com>
2026-02-26 22:45:56 +0530

    Day 1: Initial Spring Boot project setup

---
+++ costguard/src/main/java/com/prashant/costguard/CostguardApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.costguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostguardApplication.class, args);
+	}
+
+}
