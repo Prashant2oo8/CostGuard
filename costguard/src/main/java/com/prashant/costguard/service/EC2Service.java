e1ef767c7685f8494ba421b56c4c86a7d82479ad
Prashant Bhardwaj <prashantbhardwaj0107@gmail.com>
2026-03-01 23:24:49 +0530

    day 3: integrate with AWS SDk for EC2 live updadte

--- costguard/src/main/java/com/prashant/costguard/service/EC2Service.java
+++ costguard/src/main/java/com/prashant/costguard/service/EC2Service.java
@@ -2,16 +2,36 @@ package com.prashant.costguard.service;
 
 import com.prashant.costguard.model.EC2Instance;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
+import software.amazon.awssdk.services.ec2.model.Instance;
 
 import java.util.List;
 import java.util.ArrayList;
 
 @Service
 public class EC2Service {
+    private final Ec2Client ec2Client;
+
+    public EC2Service(){
+        this.ec2Client = Ec2Client.create();
+    }
+
     public List<EC2Instance> getAllInstances(){
+        DescribeInstancesResponse response = ec2Client.describeInstances();
         List<EC2Instance> instances = new ArrayList<>();
-        instances.add(new EC2Instance("i-1234", "t2.micro", "running", 10.5));
-        instances.add(new EC2Instance("i=5678", "t2.small", "stopped", 9.5));
+    for (var reservation : response.reservations()){
+        for (Instance instance: reservation.instances()){
+            instances.add(
+                    new EC2Instance(
+                            instance.instanceId(), instance.instanceTypeAsString(), instance.state().nameAsString(), 0.0
+                    )
+            );
+
+
+        }
+    }
+
         return instances;
     }
 
