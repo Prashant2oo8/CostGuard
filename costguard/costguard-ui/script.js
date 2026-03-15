a7d129fc23e73551a0979e57f2d12e5d73930466
Prashant Bhardwaj <prashantbhardwaj0107@gmail.com>
2026-03-16 03:28:26 +0530

    feat(dashboard): day 23 add service cost pie chart, resource tables, and improve chart layout styling

--- costguard/costguard-ui/script.js
+++ costguard/costguard-ui/script.js
@@ -13,7 +13,47 @@ fetch("http://localhost:8080/cloud/report")
     document.getElementById("optimizedCost").innerText =
         data.summary.optimizedMonthlyCost;
 
-})
-.catch(error => {
-    console.log("Error:", error);
+
+    // Chart Data
+    const services = Object.keys(data.serviceCostBreakdown);
+    const costs = Object.values(data.serviceCostBreakdown);
+
+    new Chart(document.getElementById("costChart"), {
+        type: 'pie',
+        data: {
+            labels: services,
+            datasets: [{
+                data: costs
+            }]
+        }
+    });
+
+
+    // Expensive Resources Table
+    const expensiveTable = document.querySelector("#expensiveTable tbody");
+
+    data.topExpensiveResources.forEach(resource => {
+        const row = `
+        <tr>
+            <td>${resource.name}</td>
+            <td>${resource.type}</td>
+            <td>${resource.monthlyCost}</td>
+        </tr>`;
+        expensiveTable.innerHTML += row;
+    });
+
+
+    // Waste Resources Table
+    const wasteTable = document.querySelector("#wasteTable tbody");
+
+    data.topWastefulResources.forEach(resource => {
+        const row = `
+        <tr>
+            <td>${resource.name}</td>
+            <td>${resource.reason}</td>
+            <td>${resource.potentialSaving}</td>
+        </tr>`;
+        wasteTable.innerHTML += row;
+    });
+
 });
