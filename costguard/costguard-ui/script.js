d2fa812e380515d562728c5e0588e95db1833d08
Prashant Bhardwaj <prashantbhardwaj0107@gmail.com>
2026-03-17 15:53:54 +0530

    feat(dashboard): day 28 implement dark mode and light mode toggle for dashboard UI

--- costguard/costguard-ui/script.js
+++ costguard/costguard-ui/script.js
@@ -100,4 +100,24 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("API Error:", error);
         });
 
+    /* THEME TOGGLE */
+
+    const toggleBtn = document.getElementById("themeToggle");
+
+    if (toggleBtn) {
+
+        toggleBtn.addEventListener("click", () => {
+
+            document.body.classList.toggle("dark");
+
+            if (document.body.classList.contains("dark")) {
+                toggleBtn.innerText = "Light Mode";
+            } else {
+                toggleBtn.innerText = "Dark Mode";
+            }
+
+        });
+
+    }
+
 });
